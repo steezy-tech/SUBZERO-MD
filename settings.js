074439bf37293ab8f0f2ea8deae9be1af95738bd
steezy-tech <stephenfawehinmi201@gmail.com>
2025-12-30 21:42:52 +0100

    Update settings.js for Veno

--- settings.js
+++ settings.js
@@ -1,8 +1,6 @@
 module.exports = {
-  SESSION_ID: "", // Put Session Id Here Inside "*"
-  OWNER_NUMBER: "263719647303", // Put Owner Number Here 
-  PREFIX: ".", // Default Prefix You Can Change To .,*!?+-&%#@
-  CDN: "https://mrfrankk-cdn.hf.space" // Dont change this part
+  SESSION_ID: "Ice~xH3I0E",
+  OWNER_NUMBER: "2347071847455", 
+  PREFIX: ".",
+  CDN: "https://mrfrankk-cdn.hf.space"
 };
-
-// CREATED BY DARRELL MUCHERI ( MR FRANK OFC 🇿🇼 )
